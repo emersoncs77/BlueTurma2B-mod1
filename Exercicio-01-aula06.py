fed08d91717d28e0cc7fa96c2e792c6a490ed865
Emerson <emersoncs77@gmail.com>
2021-05-11 22:10:30 -0300

    Exercicio 01 - Aula 06

---
+++ Exercicio-01-aula06.py
@@ -0,0 +1,13 @@
+global z
+z = 10
+
+def soma1():
+    a = int(input("Digite o primeiro valor : "))
+    b = int(input("Digite o segundo valor : "))
+    soma = a + b + z
+    return soma
+
+x = soma1()
+print(x)
+
+    
