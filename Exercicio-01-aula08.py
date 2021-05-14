e3c4be7a2b70c28d945a6e75428a0007b43ac6dc
Emerson <emersoncs77@gmail.com>
2021-05-13 22:44:34 -0300

    primeira versao

---
+++ Exercicio-01-aula08.py
@@ -0,0 +1,19 @@
+L = [5, 7, 2, 9, 4, 1 ,3]
+
+resultado = len = (L)
+print(resultado)
+
+resultado = max(L)
+print(resultado)
+
+resultado = min(L)
+print(resultado)
+
+resultado = sum(L)
+print(resultado)
+
+L.sort()
+print(L)
+
+L.reverse()
+print(L)
