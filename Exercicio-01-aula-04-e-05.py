78fea182976f9e8164e81955e11ddad7b2b4b7c7
Emerson <emersoncs77@gmail.com>
2021-05-18 22:10:52 -0300

    Duas formas de executar o mesmo código

---
+++ Exercicio-01-aula-04-e-05.py
@@ -0,0 +1,39 @@
+valor1 = int(input("Digite o primeiro valor: "))
+valor2 = int(input("Digite o segundo valor: "))
+valor3 = int(input("Digite o terceiro valor: ")) 
+
+if valor1 > valor2 > valor3:
+   print("O maior valor é: ", valor1)
+elif valor2 > valor1 > valor3:
+   print("O maior valor é: ", valor2)
+else:
+   print("O maior valor é: ", valor3)
+
+if valor1 < valor2 < valor3:
+   print("O menor valor é: ", valor1)
+elif valor2 < valor1 < valor3:
+   print("O menor valor é: ", valor2)
+else:
+    print("O menor valor é: ", valor3)
+
+
+#Código 2#
+
+# num1 = int(input("Digite o primeiro valor: "))
+# num2 = int(input("Digite o segundo valor: "))
+# num3 = int(input("Digite o terceiro valor: "))
+
+# if num1 > num2 and num1 > num3:
+#   print("O número maior é: ", num1)
+# if num2 > num1 and num2 > num3:
+#   print("O número maior é", num2)
+# if num3 > num1 and num3 > num2:
+#   print("O número maior é:", num3)
+
+
+# if num1 < num2 and num1 < num3:
+#   print("O número maior é: ", num1)
+# if num2 < num1 and num2 < num3:
+#   print("O número maior é", num2)
+# if num3 < num1 and num3 < num2:
+#   print("O número maior é:", num3)
